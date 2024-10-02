346c4b08bbb0a6fe751a9d2eec289813ff5812fb
mhuang20 <mhuang20@nait.ca>
2024-10-01 21:07:44 -0600

    Finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 // Enter your code below.
+document.addEventListener("DOMContentLoaded", function() {
+    const showResourcesBtn = document.getElementById('show-resources');
+    const resourcesDiv = document.querySelector('.javascript-resources');
+    const resourceItems = document.querySelectorAll('.list-group-item');  
+    showResourcesBtn.addEventListener('click', function() {
+      resourcesDiv.classList.remove('d-none');
+    });
+
+    resourceItems.forEach(item => {
+      item.addEventListener('mouseover', function(event) {
+        if (event.currentTarget.classList.contains('list-group-item')) {
+          event.currentTarget.classList.add('fw-bold');
+        }
+      });
+
+      item.addEventListener('mouseout', function(event) {
+        if (event.currentTarget.classList.contains('list-group-item')) {
+          event.currentTarget.classList.remove('fw-bold');
+        }
+      });
+
+      item.addEventListener('click', function(event) {
+        if (event.currentTarget.classList.contains('list-group-item')) {
+          event.currentTarget.classList.toggle('fst-italic');
+        }
+      });
+    });
+  });
